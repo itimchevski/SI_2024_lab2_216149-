50f5a96e391ecde9d602997d5ad63bd5e57df7be
Ivan <ivan.timchevski.5@outlook.com>
2024-07-05 14:33:09 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # SI_2024_lab2_216149-
+Ivan Timchevski 216149 
+~[diagram](/c/Users/Ivan/Desktop/SI_2024_lab2_216149-/diagram.png) 
+<h1>Ciklomatska kompleksnost </h2>
+<p> Ja presmetuvame slednava ravenka:M=E-N+2p = 8 <p>
+<h2> Test slucaevi spored Every Branch kriterium, ispolnet vo 4 slucai </h2>
+<p> - Prv iskluchok koga allItems = null - Test slucaj [null, "0123", "301", "0.1"] </p>
+<p> - Vtor iskuluchok koga getBarcode!=null ili dolzinata na name e 0 </p> 
+<p> - Iskluchok koga barcode e razlichno od null <p>
+<p> - Iskluchok koga sum <= payment </p>
+
+<h2> Test sluchai spored Multiple Condition kriterium - vo 2 od niv </h2>
+<p> - item.getName() == null | item.getName(),length() == 0 <p>
+<p> - item.getPrice() > 300 && item.getDiscount() > 0 && item.getBarcdoe().charAt(0) == '0'<p>
+
+<h2> Objasnuvanje za napishanite test - units </h2>
+<p> - testAllItemsNull test koga all items se null </p>
+<p> - getBarcode!=null ili dolzina na name=0 </p>
+<p> - Sluchaj koga sum <= payment </p>
+
+<h2> Tests za Every Branch: </h2>
+<p> - test za name: null || name.length==0</p>
+<p> - test koga item.getPrice()> 300 && item.getDiscount() > &&item.getBarcode().charAt(0) == '0' </p>
+
---
+++ src/main/java/SILab2.java
@@ -0,0 +1,80 @@
+import java.util.List;
+
+class Item {
+    String name;
+    String barcode; //numerical
+    int price;
+    float discount;
+
+    public Item(String name, String code, int price, float discount) {
+        this.name = name;
+        this.barcode = code;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBarcode(String code) {
+        this.barcode = code;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+}
+
+
+public class SILab2 {
+    public static boolean checkCart(List<Item> allItems, int payment){
+        if (allItems == null){
+            throw new RuntimeException("allItems list can't be null!");
+        }
+
+        float sum = 0;
+
+        for (Item item : allItems){
+            if (item.getName() == null || item.getName().length() == 0){
+                item.setName("unknown");
+            }
+            if (item.getBarcode() != null){
+                if (!item.getBarcode().matches("\\d+")) {
+                    throw new RuntimeException("Invalid character in item barcode!");
+                }
+                if (item.getDiscount() > 0){
+                    sum += item.getPrice() * item.getDiscount();
+                } else {
+                    sum += item.getPrice();
+                }
+            } else {
+                throw new RuntimeException("No barcode!");
+            }
+            if (item.getPrice() > 300 && item.getDiscount() > 0 && item.getBarcode().charAt(0) == '0'){
+                sum -= 30;
+            }
+        }
+        return sum <= payment;
+    }
+}
